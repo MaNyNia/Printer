84440dcc3b7223db551f47114ea76ebf802f9202
Marko <ilya1501@gmail.com>
2022-09-29 11:32:40 +0300

    properties

---
+++ Printers/Shape.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Printers
+{
+   public abstract class Shape
+    {
+        public char Symbol { get; }
+        public int XAxis { get; set; }
+        public int YAxis { get; set; }
+        public int Side { get; }
+    }
+
+}
